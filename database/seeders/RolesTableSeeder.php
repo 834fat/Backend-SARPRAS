4ef9e586fac689af633ee27dd937e9309d7bf79b
834fat <fatihmuttaqein211@gmail.com>
2024-01-21 09:04:51 +0700

    first commit

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Database\Seeders\PermissionTableSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            'name' => 'admin',
+            'guard_name' => 'api'
+        ]);
+
+        Role::create([
+            'name' => 'user',
+            'guard_name' => 'api'
+        ]);
+    }
+}
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('user'),
+        ]);
+        $user->assignRole('user');
+
+        $role = Role::find(1);
+        $permissions = Permission::all();
+
+        $role->syncPermissions($permissions);
+
+        //assign role with permission to user
+        $user = User::find(1);
+        $user->assignRole($role->name);
+    }
+}
